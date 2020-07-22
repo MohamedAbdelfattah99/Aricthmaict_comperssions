a413943dea6d3310a9f629d168e60a6012724bff
MohamedAbdelfattah99 <67230191+MohamedAbdelfattah99@users.noreply.github.com>
2020-07-22 14:41:28 +0200

    Add files via upload

---
+++ Symbols.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aricthmaict;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Symbols {
+   double low,high,range, freq,lower,upper;
+    char symbol;
+    
+
+    public Symbols() {
+        low=0;
+        high=0;
+        range=0;
+        freq=0;
+        lower=0;
+        upper=0;
+        symbol=' ';
+    }
+    
+}
