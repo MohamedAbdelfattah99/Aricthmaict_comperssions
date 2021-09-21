37eaea4b524f0de7e1b1980a4f3d2d087d30a387
MohamedAbdelfattah99 <67230191+MohamedAbdelfattah99@users.noreply.github.com>
2021-09-21 18:54:15 +0200

    Create main.R

---
+++ main.R
@@ -0,0 +1,4 @@
+# Aricthmaict_comperssions
+ ● Arithmetic Coding : Arithmetic coding completely by passes the idea of replacing an input symbol with a specific code. Instead,
+ it takes a stream of input symbols and replaces
+ it with a single floating point number The longer and more complex the message, the more bits are needed to represents the output number.
